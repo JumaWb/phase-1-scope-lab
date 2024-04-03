90ddf8b56de6530a625809c942485ce2093b45eb
Brian Juma <bjuma0985@gmail.com>
2024-04-03 15:19:02 +0300

    scope

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // Write your solution in this file!
-var customerName = "bob";
+var customerName = 'bob';
 
-function upperCaseCustomerName(){
-    return customerName.toUpperCase();
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
 }
+
 function setBestCustomer() {
     bestCustomer = 'not bob';
-  }
-function overwriteBestCustomer() {
-    bestCustomer = "maybe bob"
 }
 
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'George';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Eve';
+   
+}
 
 
